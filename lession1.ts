5bea0f5a7f1490611805e0a2723934741bc2f27b
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2021-10-05 00:48:28 -0400

    initial code for typescript basics

---
+++ lession1.ts
@@ -0,0 +1,20 @@
+
+// A typescript function that when called logs the message to the console.
+function log(message){
+    console.log(message);
+}
+
+var message = "hello world";
+
+log(message);
+
+// Create a function that iterate through a fix number of elements and produces a result
+function  doSomething(){
+    for(var i = 0; i< 5; i++){
+        console.log(i);
+    }
+
+    console.log('Finally the result of i : ', i)
+}
+
+doSomething();
