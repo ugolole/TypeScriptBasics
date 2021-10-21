37933e9f1e9df65b1e791ee89d3c4cee9c5000ed
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2021-10-21 02:58:31 -0400

    Updating lesson3 to include arrow functions and interfaces.

---
+++ lesson3.ts
@@ -0,0 +1,45 @@
+﻿// Declaring functions in js
+let jsFunc = function (message){
+    console.log(message);
+}
+
+// declaring in ts
+let tsFunc = (message : string) =>{
+    console.log(message);
+}
+
+// using the declared functions above.
+jsFunc('js function message');
+tsFunc('ts function message');
+
+// declaring a much shorter function also known as arrow function.
+let tsShorterFunc = (message) => console.log(message);
+
+// calling ts shorter function
+tsShorterFunc('ts shorter function')
+
+// arrow function without parameter
+let tsArrowFunc = () => console.log('arrow function without parameters');
+
+tsArrowFunc();
+
+// creating custom types
+// Inline annotation.
+let drawPoint = (point : { x: number, y: number} ) => {
+    // to do.
+}
+
+// creating an interface of points.
+interface Point{
+    x: number,
+    y: number
+}
+
+let otherDrawPoint = (point : Point) =>{
+    // to do
+}
+
+// other example a function that calculates the distance between points.
+let distanceBetweenPoint = (pointA: Point, pointB: Point) => {
+    // to do
+}
