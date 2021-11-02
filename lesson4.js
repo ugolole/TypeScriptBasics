7066627ccf84080d64c4e6a74bfd475f6756b4cd
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2021-11-02 17:18:33 -0400

    Adding a class and it's required members.

--- lesson4.js
+++ lesson4.js
@@ -1,18 +1,11 @@
-﻿// In this lesson we will look at classes.
-
+// In this lesson we will look at classes.
 // Everything about a point is in class.
 class Point {
-    
-    // fields.
-    x: number;
-    y: number;
-    
     // methods.
-    draw(){
+    draw() {
         // .. logic used to draw a point.
     }
-    
-    getDistance(){
+    getDistance() {
         // .. logic used to get the distance of a point.
     }
 }
