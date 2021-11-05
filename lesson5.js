9703e7d891c595b6b8974da2053687324c7e4794
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2021-11-04 23:18:52 -0400

    Minor updates to typescript

---
+++ lesson5.js
@@ -0,0 +1,10 @@
+class OtherPointClass {
+    Draw() {
+        console.log("Drawing something using the point class.");
+    }
+    getDistance() {
+        console.log('Getting the distance using the point class.');
+    }
+}
+let point = new OtherPointClass();
+point.Draw();
---
+++ lesson5.ts
@@ -0,0 +1,17 @@
+﻿class OtherPointClass {
+    
+    x: number;
+    y: number;
+    
+    Draw(){
+        console.log("Drawing something using the point class.")    
+    }
+    
+    getDistance(){
+        console.log('Getting the distance using the point class.')
+    }
+}
+
+let point : OtherPointClass = new OtherPointClass();
+
+point.Draw();
