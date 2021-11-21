18da61d207e3bb51e7a7cc94604c6b4f6b9a3802
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2021-11-20 23:42:53 -0500

    Learning about import and exports in angular

---
+++ lesson7.js
@@ -0,0 +1,3 @@
+import { Person } from "./person";
+let person = new Person("Ivan Richard", 1, 2, 3);
+console.log("peron's name is :", person.name);
---
+++ person.ts
@@ -0,0 +1,25 @@
+﻿// An implementation of the person object.
+export class Person{
+    
+    // A person constructor with optional properties
+    constructor(private _name?: string,
+                private _age ?:number,
+                private _height?:number,
+                private _weight?:number) {
+    }
+    
+    // Get the value for the name of this person.
+    get name(){
+        return this._name;
+    }
+    
+    // Set the name of this person object.
+    set name(value:string){
+        if(value.length == 0){
+            throw new Error("value can't be empty")
+        }
+        this._name = value;
+    }
+    
+    // TODO: - add all the needed setters and getters for the private properties of this person class.
+}
